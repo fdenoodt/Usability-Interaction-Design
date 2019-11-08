d3bcf83c8cf9dafe7c5e489179d815484c750d2c
fabian.denoodt <fabian.denoodt@student.odisee.be>
2019-11-08 12:50:40 +0100

    communicate to python api from unity

---
+++ unity/unity python communication/Assets/scripts/DoSomething.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class DoSomething : MonoBehaviour
+{
+
+    static HttpClient c = new HttpClient();
+    async void Start()
+    {
+        Debug.Log("Script has started!");
+        var res = await GetAsync("http://127.0.0.1:5000/?length=100&power=33");
+        Debug.Log(res.ToString());
+    }
+
+    void Update()
+    {
+    }
+
+    static async Task<string> GetAsync(string path)
+    {
+        string result = "";
+        HttpResponseMessage response = await c.GetAsync(path);
+        if (response.IsSuccessStatusCode)
+        {
+            result = await response.Content.ReadAsStringAsync();
+        }
+        return result;
+    }
+}
